d7a3e11ed1cc280105cf9e65f39c54a3509cd8d9
rishi6670 <rishi6670@gmail.com>
2019-11-14 16:29:33 +0530

    second commit

--- app.js
+++ app.js
@@ -1,70 +1,43 @@
 const express = require('express');
 const chalk = require('chalk');
 const path = require('path');
-
+const bodyparser = require('body-parser');
+const cors = require('cors');
 var app = new express(); //creating a constructor for express object
-const booksRouter = express.Router();
+var nav = [{link:'/',title:'Home'},
+            {link:'/signup',title:'Signup'},
+            {link:'/login',title:'Login'},
+            {link:'/books',title:'Books'},
+            {link:'/authors',title:'Authors'},
+            {link: '/books/add',title: 'Add Book'}
+        ];
+var authorRouter = require('./src/routes/authorRoutes')(nav);
+var booksRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static(path.join(__dirname,"/public"))); // to recieve sttaic files from public
+
+app.use(bodyparser.json());
+app.use(cors());
+app.use(bodyparser.urlencoded({extended:true}));
+
 app.use('/books',booksRouter);
+app.use('/authors',authorRouter);
+app.use('/books/add',booksRouter);
+
 app.set('views','./src/views');
 app.set('view engine','ejs');
-var nav = [{link:'/',title:'Home'},
-        {link:'/signup',title:'Signup'},
-        {link:'/login',title:'Login'},
-        {link:'/books',title:'Books'},
-        {link:'/authors',title:'Authors'}]
-var books = [
-    {
-        title: "War and Peace",
-        genre: "Historical Fiction",
-        author: "Lev Nicola",
-        image: "book1.jpg"
-    },
-    {
-        title: "Les Miserables",
-        genre: "Historical Fiction",
-        author: "Victor Hugo",
-        image:"book2.jpg"
-    },
-    {
-        title: "Harry Potter",
-        genre: "Magical Fiction",
-        author: "J.K Rowling",
-        image:"book3.jpg"
-    },
-    {
-        title: "End of the Beginning",
-        genre: "Drama",
-        author: "Harry weslie",
-        image:"book4.jpg"
-    }
-];
-
-booksRouter.route('/').get((req,res)=>{
-    res.render('books',
-        {nav,
-        title:"Books",
-        books})
-    });
 
 app.get('/',(req,res)=>{
     // res.render('Index.ejs',{list:['book1','book2','book3'],title:"Library"});
-    res.render('Index.ejs', {nav:[{link:'/',title:'Home'},
-                                {link:'/signup',title:'Signup'},
-                                {link:'/login',title:'Login'},
-                                {link:'/books',title:'Books'},
-                                {link:'/authors',title:'Authors'}],
-                                title:"Library"
-    }
+    res.render('Index.ejs', {nav, title:"Library"}
                             
     );
 });
-app.get('/',(req,res)=>{
-    // res.send('This is my first  application')
-//     res.sendFile(__dirname+"/src/views/Index.html");
-    res.sendFile(path.join(__dirname,"/src/views/Index.html"));
-})
-app.listen(3800,()=>{
-    console.log(chalk.yellow('Listening to port 3800'));
+// app.get('/',(req,res)=>{
+//     // res.send('This is my first  application')
+// //     res.sendFile(__dirname+"/src/views/Index.html");
+//     res.sendFile(path.join(__dirname,"/src/views/Index.html"));
+// })
+app.listen(4000,()=>{
+    console.log(chalk.yellow('Listening to port 4000'));
 }); // selecting the port
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,64 @@
+var express = require('express')
+var booksRouter = express.Router();
+
+var books = [
+    {
+        title: "War and Peace",
+        genre: "Historical Fiction",
+        author: "Lev Nicola",
+        image: "book1.jpg",
+        content: "In Les Misérables, Jean Valjean spends nineteen"
+    },
+    {
+        title: "Les Miserables",
+        genre: "Historical Fiction",
+        author: "Victor Hugo",
+        image:"book2.jpg",
+        content: "In Les Misérables, Jean Valjean spends nineteen years in prison for stealing a loaf of bread. After release, he's plagued by a suspicious police inspector, Javert. Valjean and his adopted daughter Cosette face much hardship, but finally find peace at the end of the novel."
+    },
+    {
+        title: "Harry Potter",
+        genre: "Magical Fiction",
+        author: "J.K Rowling",
+        image:"book3.jpg",
+        content: "Adaptation of the first of J.K. Rowling's popular children's novels about Harry Potter, a boy who learns on his eleventh birthday that he is the orphaned son of two powerful wizards and possesses unique magical powers of his own. He is summoned from his life as an unwanted child to become a student at Hogwarts, an English boarding school for wizards. There, he meets several friends who become his closest allies and help him discover the truth about his parents' mysterious deaths."
+
+    },
+    {
+        title: "End of the Beginning",
+        genre: "Drama",
+        author: "Harry weslie",
+        image:"book4.jpg",
+        content: "Adaptation of the first of J.K. Rowling's popular"
+    }
+];
+function router(nav){
+booksRouter.route('/').get((req,res)=>{
+    res.render('books',
+        {nav,
+        title:"Books",
+        books})
+    });
+    booksRouter.route('/add').get((req,res)=>{
+        const id = req.params.id;
+        res.render('add',
+            {nav,
+            title:"Add Book"
+        })
+        }); 
+    booksRouter.route('/save').post((req,res)=>{
+      res.header('Access-Control-Allow-Origin');
+      res.header('Access-Control-Allow-Methods: GET,POST,PATCH,PUT,DELETE,OPTIONS');
+        console.log(req.body);
+        }); 
+    booksRouter.route('/:id').get((req,res)=>{
+        const id = req.params.id;
+        res.render('book',
+            {nav,
+            title:"Book",
+            book: books[id]})
+        });  
+        return booksRouter;
+  
+}  
+module.exports = router;
